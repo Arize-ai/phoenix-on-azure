2000f9b863af7439bcbe0c6dc73a6cbc1cb5afa3
Yann-Gaël Gautheron <1979302+abalam666@users.noreply.github.com>
2024-07-10 19:39:39 +0200

    fix: improve error message when missing python (#9)
    Co-authored-by: Yann-Gael Gautheron <abalam666@users.noreply.github.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-python ./scripts/init.py
+PYTHON_CMD="python"
+
+if ! command -v python &> /dev/null
+then
+    if ! command -v python3 &> /dev/null
+    then
+        echo "python could not be found."
+        exit 1
+    else
+        PYTHON_CMD="python3"
+    fi
+fi
+
+$PYTHON_CMD ./scripts/init.py
--- scripts/post.sh
+++ scripts/post.sh
@@ -1,2 +1,15 @@
- #!/bin/sh
-python ./scripts/banner.py
+#!/bin/sh
+PYTHON_CMD="python"
+
+if ! command -v python &> /dev/null
+then
+    if ! command -v python3 &> /dev/null
+    then
+        echo "python could not be found."
+        exit 1
+    else
+        PYTHON_CMD="python3"
+    fi
+fi
+
+$PYTHON_CMD ./scripts/banner.py
